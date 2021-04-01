24930a5bf7e7ed0e89b4da6c55ee369042df17f8
costamay <titusouko@gmail.com>
2021-04-01 16:33:58 +0300

    Add function to handle key press

--- script.js
+++ script.js
@@ -11,4 +11,45 @@ let decimalAdded = false;
 
 const operators = ["+", "-", "x", "÷"];
 
+function handleKeyPress (e) {
+  const key = e.target.dataset.key;
+  const lastChar = operation[operation.length - 1];
+
+  if (key === "=") {
+    return;
+  }
+
+  if (key === "." && decimalAdded) {
+    return;
+  }
+
+  if (operators.indexOf(key) !== -1) {
+    decimalAdded = false;
+  }
+
+  if (operation.length === 0 && key === "-") {
+    operation += key;
+    input.innerHTML = operation;
+    return;
+  }
+
+  if (operation.length === 0 && operators.indexOf(key) !== -1) {
+    input.innerHTML = operation;
+    return;
+  }
+
+  if (operators.indexOf(lastChar) !== -1 && operators.indexOf(key) !== -1) {
+    operation = operation.replace(/.$/, key);
+    input.innerHTML = operation;
+    return;
+  }
+
+  if (key) {
+    if (key === ".") decimalAdded = true;
+    operation += key;
+    input.innerHTML = operation;
+    return;
+  }
+
+}
 
